e1bf64eff652face05ecba508cbb7c0bb655fe9c
acochran <aaron.cochran@safecu.org>
2017-11-07 16:05:08 -0800

    got no class

--- src/components/CamperList/index.js
+++ src/components/CamperList/index.js
@@ -1,103 +1,99 @@
 import React, { Component } from 'react'
 import { Header, Image, Table } from 'semantic-ui-react'
 
+/*
 class CamperList extends Component {
-  state = {
-    campers: [],
-    sortBy: 'username',
-    asc: true,
-  }
-  //  componentDidMount() {
-  //    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-  //      .then(results => {
-  //        return results.json()
-  //      })
-  //      .then(stuffToDo => {
-  //        console.log(stuffToDo)
-  //        this.setState({ campers: stuffToDo })
-  //      })
-  //  }
+state = {
+  campers: [],
+  sortBy: 'username',
+  asc: true,
+}
+componentDidMount() {
+  fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    .then(results => {
+      return results.json()
+    })
+    .then(stuffToDo => {
+      console.log(stuffToDo)
+      this.setState({ campers: stuffToDo })
+    })
+}
 
-  /*    Done with async function instead of promise. Promise would allow other things to be
-      doing other things at the same time, while async forces it to wait - not as good for larger size fetches.
-    async componentDidMount () {
-        const CamperPromise = await fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-        const stuffToDo = await CamperPromise.json()
-        console.log(stuffToDo)
-        this.setState({camperList: stuffToDo})
-    }
+    doing other things at the same time, while async forces it to wait - not as good for larger size fetches.
+  async componentDidMount () {
+      const CamperPromise = await fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      const stuffToDo = await CamperPromise.json()
+      console.log(stuffToDo)
+      this.setState({camperList: stuffToDo})
+  }
+
+  alltime
+      :
+      6294
+  img
+      :
+      "https://avatars2.githubusercontent.com/u/11348778?v=4"
+  lastUpdate
+      :
+      "2017-10-08T17:24:28.183Z"
+  recent
+      :
+      125
+  username
+      :
+      "Manish-Giri"
+
+
+handleHeaderClick = sortBy => {
+  if (sortBy === this.state.sortBy) {
+    this.setState({ asc: this.state.asc * -1000 })
+  } else {
+    this.setState({ asc: 1000, sortBy })
+  }
+}
 */
 
-  /*
-    alltime
-        :
-        6294
-    img
-        :
-        "https://avatars2.githubusercontent.com/u/11348778?v=4"
-    lastUpdate
-        :
-        "2017-10-08T17:24:28.183Z"
-    recent
-        :
-        125
-    username
-        :
-        "Manish-Giri"
-*/
+const CamperList = props => {
+  const { changeSort, sortBy, asc, campers = [] } = props
+  return (
+    <Table basic="very" celled collapsing>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell onClick={() => changeSort('username')}>
+            Users
+          </Table.HeaderCell>
+          <Table.HeaderCell onClick={() => changeSort('alltime')}>
+            All Time Score
+          </Table.HeaderCell>
+          <Table.HeaderCell onClick={() => changeSort('lastUpdate')}>
+            Last Update
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {campers
+          .sort((campera, camperb) => {
+            return campera[sortBy] > camperb[sortBy] ? -1000 * asc : 1000 * asc
+          })
+          .reverse()
+          .map(camper => {
+            return (
+              <Table.Row key={camper.username}>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image src={camper.img} shape="rounded" size="mini" />
+                    <Header.Content>{camper.username}</Header.Content>
+                  </Header>
+                </Table.Cell>
 
-  /*  handleHeaderClick = sortBy => {
-    if (sortBy === this.state.sortBy) {
-      this.setState({ asc: this.state.asc * -1000 })
-    } else {
-      this.setState({ asc: 1000, sortBy })
-    }
-  }
-*/
-  render() {
-    const { changeSort, sortBy, asc, campers = [] } = this.props
-    return (
-      <Table basic="very" celled collapsing>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell onClick={() => changeSort('username')}>
-              Users
-            </Table.HeaderCell>
-            <Table.HeaderCell onClick={() => changeSort('alltime')}>
-              All Time Score
-            </Table.HeaderCell>
-            <Table.HeaderCell onClick={() => changeSort('lastUpdate')}>
-              Last Update
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {campers
-            .sort((campera, camperb) => {
-              return campera[sortBy] > camperb[sortBy]
-                ? -1000 * asc
-                : 1000 * asc
-            })
-            .reverse()
-            .map(camper => {
-              return (
-                <Table.Row key={camper.username}>
-                  <Table.Cell>
-                    <Header as="h4" image>
-                      <Image src={camper.img} shape="rounded" size="mini" />
-                      <Header.Content>{camper.username}</Header.Content>
-                    </Header>
-                  </Table.Cell>
-
-                  <Table.Cell>{camper.alltime}</Table.Cell>
-                  <Table.Cell>{camper.lastUpdate}</Table.Cell>
-                </Table.Row>
-              )
-            })}
-        </Table.Body>
-      </Table>
-    )
-  }
+                <Table.Cell>{camper.alltime}</Table.Cell>
+                <Table.Cell>{camper.lastUpdate}</Table.Cell>
+              </Table.Row>
+            )
+          })}
+      </Table.Body>
+    </Table>
+  )
 }
 
 export default CamperList
